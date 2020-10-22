9e21228d7a16d30bbe3a32e4b6080de51170c067
sarathkumarmarimuthu <sarath2763@gmail.com>
2020-10-22 16:48:47 +0530

    add code comments

--- app/src/app/services/enrollee.service.ts
+++ app/src/app/services/enrollee.service.ts
@@ -1,21 +1,24 @@
-import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
-import { Enrollee } from '../models/enrollee';
-import { enrolleesUrls } from '../urls';
+import {Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Observable} from 'rxjs';
+import {Enrollee} from '../models/enrollee';
+import {enrolleesUrls} from '../urls';
 
-@Injectable({ providedIn: 'root' })
+@Injectable({providedIn: 'root'})
 export class EnrolleeService {
   constructor(private http: HttpClient) {}
 
+  // get call to fetch all enrollees
   getAllEnrollees(): Observable<Enrollee[]> {
     return this.http.get<Enrollee[]>(enrolleesUrls);
   }
 
+  // get call to fetch individual enrollee by id
   getEnrolleeById(id: string): Observable<Enrollee> {
     return this.http.get<Enrollee>(enrolleesUrls + `/${id}`);
   }
 
+  // put call to update individual enrollee details
   updateEnrollee(enrollee: Enrollee): Observable<Enrollee> {
     return this.http.put<Enrollee>(enrolleesUrls + `/${enrollee.id}`, enrollee);
   }
